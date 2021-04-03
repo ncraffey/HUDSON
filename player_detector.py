e9df6c4b8b0d5dadce4f986ff7f1b6513437d59b
Nick Craffey <craffeyni@gmail.com>
2021-04-03 19:12:35 -0400

    djakfdsjfkajsdfj

--- player_detector.py
+++ player_detector.py
@@ -4,16 +4,12 @@ import numpy as np
 def draw_rectangle(draw_point, size):
 	cv2.rectangle(clip, draw_point, (draw_point[0] + size, draw_point[1] + size), (0, 0, 255), 0)
 
-def process_image(frame):
+def threshold_image(frame):
 	orig = frame
 
-	gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-	blur = cv2.blur(gray, ksize=(25,25))
-	_, binary = cv2.threshold(blur, 15, 255, cv2.THRESH_BINARY)
-	contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-	frame = cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)
-	
-	return frame
+	ret,thresh1 = cv2.threshold(frame,125,255,cv2.THRESH_BINARY)
+
+	return thresh1
 
 while True:
 	closed = False
@@ -22,9 +18,10 @@ while True:
 
 	while True:
 		success, clip = cap.read()
+
 		if success:
-			result = process_image(clip)
-			cv2.imshow("clip", result)
+			clip = threshold_image(clip)
+			cv2.imshow("clip", clip)
 
 			if cv2.waitKey(1) & 0xFF == ord('q'):
 				closed = True
